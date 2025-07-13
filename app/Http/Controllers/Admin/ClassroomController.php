9b2a1e14e133a7f6bbd16a24bb9605c94f3c8f1a
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2025-07-13 14:53:02 +0300

    ClassRoom Controller

--- app/Http/Controllers/Admin/ClassroomController.php
+++ app/Http/Controllers/Admin/ClassroomController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Helpers\Helpers;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Classrooms\StoreClassrommRequest;
+use App\Http\Requests\Classrooms\UpdateClassrommRequest;
 use App\Models\Classroom;
 use Illuminate\Http\Request;
 
@@ -22,35 +24,59 @@ class ClassroomController extends Controller
     }
 
 
+
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreClassrommRequest $request)
     {
-        //
+        $data = $request->validated();
+        $classroom = Classroom::create($data);
+        if ($classroom)
+        {
+            return Helpers::sendResponse(200, 'Classroom Created', $classroom);
+        }
+        return Helpers::sendResponse(400, 'Something Went Wrong', []);
     }
+
+
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Classroom $classroom)
     {
-        //
+        if($classroom)
+        {
+            return Helpers::sendResponse(200, 'Classroom Retrieved Successfully', $classroom);
+        }
+        return Helpers::sendResponse(400, 'Something Went Wrong', []);
     }
 
+
+    
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateClassrommRequest $request, Classroom $classroom)
     {
-        //
+        $data = $request->validated();
+        $update = $classroom->update($data);
+        if ($update)
+        {
+            return Helpers::sendResponse(200, 'Classroom Updated Successfully', $classroom);
+        }
+        return Helpers::sendResponse(400, 'Something Went Wrong', []);
     }
+
+
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Classroom $classroom)
     {
-        //
+        $classroom->delete();
+        return Helpers::sendResponse(200, 'Classroom Deleted Successfully', []);
     }
 }
---
+++ app/Http/Requests/Classrooms/StoreClassrommRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests\Classrooms;
+
+use App\Helpers\Helpers;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class StoreClassrommRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return in_array($this->user()->role, ['admin']);
+    }
+
+
+
+    /**
+     * Failed Validation
+     */
+    public function failedValidation(\Illuminate\Contracts\Validation\Validator $validator)
+    {
+        $res = Helpers::sendResponse(422, 'Validation Errors', $validator->errors());
+        throw new ValidationException($validator, $res);
+    }
+
+
+
+    /**
+     * Failed Authorization
+     */
+    public function failedAuthorization()
+    {
+        $res = Helpers::sendResponse(403, 'You are not Authorized!!!', []);
+        return new AuthorizationException($res);
+    }
+
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'          => 'required|string|max:255',
+            'description'   => 'nullable|string',
+        ];
+    }
+}
